a878f4e8ae64b0afd365276648c7025416218fc9
Ariamb <ariambartsch@gmail.com>
2023-07-11 01:37:21 -0300

    first version dotproduct with cuda

---
+++ benchmarks/dp.ex
@@ -0,0 +1,71 @@
+defmodule DP do
+  import GPotion
+
+
+gpotion dotproduct(a,b,c) do
+
+	tid = threadIdx.x + blockIdx.x * blockDim.x
+	cacheIndex = threadIdx.x
+  temp = 0.0
+
+  while (tid < N) do
+    temp = a[tid] * b[tid] + temp
+		tid = blockDim.x * gridDim.x + tid
+  end
+  cache[cacheIndex] = temp
+  __syncthreads()
+
+  while (i != 0) do
+		if (cacheIndex < i) do
+			cache[cacheIndex] = cache[cacheIndex + i] + cache[cacheIndex]
+		__syncthreads()
+    end
+		i = i/2
+	end
+
+end
+
+
+n = 33 * 1024 #constante de tamanho q to usando
+
+
+#só pra encher vetor
+def fill_array(a, b, n, n) do 
+      {a, b}
+end
+def fill_array(a, b, i, n) do
+      fill_array(Matrex.set(a, 1, i + 1, i), Matrex.set(b, 1, i + 1, i), i+1, n)
+end
+  
+
+
+a = Matrex.ones(1, n)
+b = Matrex.ones(1, n)
+c = Matrex.ones(1, n)
+
+{a, b} = fill_array(a, b, 0, n)
+
+
+threadsPerBlock = 256;
+numberOfBlocks = trunc((n + threadsPerBlock - 1)/threadsPerBlock)
+
+
+prev = System.monotonic_time()
+
+kernel = GPotion.load(&DP.dotproduct/3)
+a1 = GPotion.new_gmatrex(a)
+b1 = GPotion.new_gmatrex(b)
+c1 = GPotion.new_gmatrex(c)
+
+GPotion.spawn(kernel,{numberOfBlocks,1,1},{threadsPerBlock,1,1},[a1,b1,c1])
+
+GPotion.synchronize()
+
+_result = GPotion.get_gmatrex(c1)
+
+next = System.monotonic_time()
+#IO.puts "time gpu #{System.convert_time_unit(next-prev,:native,:millisecond)}"
+IO.puts "GPotion\t#{m}\t#{System.convert_time_unit(next-prev,:native,:millisecond)} "
+
+IO.inspect result
+#IO.puts GPU.Backend.gen_c_kernel('addVectors',4,[])
