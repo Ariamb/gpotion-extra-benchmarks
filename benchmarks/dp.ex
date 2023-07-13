be0f290f1d537fd27e2e7d0ea8cc12b747d70c69
Ariamb <ariambartsch@gmail.com>
2023-07-12 21:05:22 -0300

    testing memcopy

--- benchmarks/dp.ex
+++ benchmarks/dp.ex
@@ -96,9 +96,10 @@ GPotion.synchronize()
 #IO.puts "time gpu #{System.convert_time_unit(next-prev,:native,:millisecond)}"
 
 #resultfake = GPotion.get_gmatrex(ref3)
-FUNC.soma_array(resultreal, 0)
+
 resultreal = GPotion.get_gmatrex(ref3)
-IO.puts("rel")
+s = FUNC.soma_array(resultreal, 0)
+IO.puts("rel, sum = #{s}")
 IO.inspect resultreal
 #IO.puts("fake")
 #IO.inspect resultfake
