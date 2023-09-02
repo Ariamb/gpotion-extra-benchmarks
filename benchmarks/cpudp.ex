95887ad230defe12db3f02d8f9008610c35fd24b
Ariamb <ariambartsch@gmail.com>
2023-09-02 12:00:19 -0300

    add cpudotproduct
    added dotproduct calculation that runs in the cpu, using the matrex library. Added time measurement and file writting with time logs for cpudp. todo: time measurements and logging for other alghoritms.

---
+++ benchmarks/cpudp.ex
@@ -0,0 +1,35 @@
+import Matrex
+
+defmodule Utils do
+    import Matrex
+
+    def fill_array(a, b, n, n) do 
+        {a, b}
+    end
+
+
+    def fill_array(a, b, i, n) do
+        fill_array(Matrex.set(a, 1, i + 1, i), Matrex.set(b, 1, i + 1, i), i+1, n)
+    end
+    
+  end
+  
+
+#n = 33 * 1024
+{n, _} = Integer.parse(Enum.at(System.argv, 0))
+{iteration, _} = Integer.parse(Enum.at(System.argv, 1))
+
+
+a = Matrex.ones(1, n)
+b = Matrex.ones(1, n)
+
+prev = System.monotonic_time()
+{a, b} = Utils.fill_array(a, b, 0, n)
+next = System.monotonic_time()
+c = Matrex.dot_nt(a, b)
+IO.inspect(c)
+
+text = "time: #{next - prev}, iteration: #{iteration}, array size: #{n} \n"
+File.write!("time-cpudp.txt", text, [:append])
+
+
