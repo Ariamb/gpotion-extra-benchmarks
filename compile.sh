a2b564a5d668e5d434b45876f8d1fce00fa3fa9e
ambartsch <Ariamb>
2023-09-04 16:24:04 -0300

    patch benchmark

--- compile.sh
+++ compile.sh
@@ -3,4 +3,4 @@ mix deps.get
 mix compile
 
 nvcc -o c_implementations/craytracer c_implementations/craytracer.cu
-nvcc -o c_implementations/dotproduct c_implementations/dotproduct.cu
+nvcc -o c_implementations/dotproduct c_implementations/cdotproduct.cu
