901966de20709091c36827a4bb3730298fc4370d
Ariam Moresco Bartsch <ariambartsch@gmail.com>
2023-09-06 21:33:59 -0300

    code formatting

--- benchmarks/cpumatrexdp.ex
+++ benchmarks/cpumatrexdp.ex
@@ -1,6 +1,5 @@
 import Matrex
 
-
 defmodule Utils do
     def dot_product([],[], sum) do
         sum
@@ -10,20 +9,19 @@ defmodule Utils do
     end
 end 
 
-
 {n, _} = Integer.parse(Enum.at(System.argv, 0))
 {iteration, _} = Integer.parse(Enum.at(System.argv, 1))
-
 
 a = Matrex.new([Enum.to_list(1..n)])
 b = Matrex.new([Enum.to_list(1..n)])
 
-IO.puts("começando a calcular")
+IO.puts("Começando o cálculo de dot product")
+
 prev = System.monotonic_time()
 c = Matrex.dot_nt(a, b)
 next = System.monotonic_time()
-IO.puts("terminei de calcular")
-IO.inspect(c)
+
+IO.puts("Cálculo finalizado. Resultado: #{c}")
 
 text = "time: #{System.convert_time_unit(next - prev,:native,:microsecond)} \t iteration: #{iteration} \t array size: #{n} \n"
 File.write!("time-matrex-cpudp.txt", text, [:append])
